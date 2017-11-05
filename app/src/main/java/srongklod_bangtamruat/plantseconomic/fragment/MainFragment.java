1325c8191ca353b9fd1d6b7435ee85be89bc0459
MasterUNG <phrombutr@gmail.com>
2017-11-05 11:59:09 +0700

    Create MainFragment Success

--- app/src/main/java/srongklod_bangtamruat/plantseconomic/fragment/MainFragment.java
+++ app/src/main/java/srongklod_bangtamruat/plantseconomic/fragment/MainFragment.java
@@ -1,8 +1,26 @@
 package srongklod_bangtamruat.plantseconomic.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import srongklod_bangtamruat.plantseconomic.R;
+
 /**
  * Created by masterung on 5/11/2017 AD.
  */
 
-public class MainFragment {
-}
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}   // Main Class
