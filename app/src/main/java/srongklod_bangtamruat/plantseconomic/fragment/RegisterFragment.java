e8eeb8310263421061f7afe783a61cf374f62a34
MasterUNG <phrombutr@gmail.com>
2017-11-05 15:01:03 +0700

    finish 5nov

--- app/src/main/java/srongklod_bangtamruat/plantseconomic/fragment/RegisterFragment.java
+++ app/src/main/java/srongklod_bangtamruat/plantseconomic/fragment/RegisterFragment.java
@@ -1,8 +1,24 @@
 package srongklod_bangtamruat.plantseconomic.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import srongklod_bangtamruat.plantseconomic.R;
+
 /**
  * Created by masterung on 5/11/2017 AD.
  */
 
-public class RegisterFragment {
-}
+public class RegisterFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        return view;
+    }
+}   // Main Class
